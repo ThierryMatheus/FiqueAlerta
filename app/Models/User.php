7392a7445d3b373a878c5df96f1c03a2b6045937
Huanderson Lima <mattewlima16@gmail.com>
2021-11-17 11:34:12 -0300

    GH-14 - Criação do model Company_profile e relacionamento com entidade user

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function company_profile()
+    {
+        return $this->hasOne(App\Models\company_profile);
+    }
 }
