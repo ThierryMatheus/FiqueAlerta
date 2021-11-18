5c7566fc865a667b8feb9912e37faf5a9314f13f
Huanderson Lima <mattewlima16@gmail.com>
2021-11-17 22:34:09 -0300

    GH-25 - Criação do model Complaint e seu relacionamento com a tabela User

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
         return $this->hasOne(user_profile::class);
     }
 
+    public function complaint()
+    {
+        return $this->hasMany(complaint::class);
+    }
+
 }
