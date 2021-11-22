7c0729d8b29c175965ba0ba97a45bb383cb20a23
HeitorGomesIF <heitorgomes500@outlook.com>
2021-11-22 18:59:21 -0300

    GH-5- Estilização básica da página de Read

--- resources/views/denuncia/show.blade.php
+++ resources/views/denuncia/show.blade.php
@@ -7,7 +7,38 @@
         </h2>
     </x-slot>
 
-    <div class="min-w-0 flex-auto px-4 sm:px-6 xl:px-8 pt-10 pb-24 lg:pb-16">
+
+    <div class="flex flex-col max-w-5xl mx-auto mt-5">
+       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+           <div class="bg-white rounded shadow p-3">
+            <table class="mx-auto border border-gray-200 rounded w-full">
+                <thead>
+                    <tr class="bg-gray-200">
+                        <th class="px-2">Denúncia</th>
+                        <th class="px-2">Comentário</th>
+                        <th class="px-2">Latitude</th>
+                        <th class="px-2">Longitude</th>
+                        <th class="px-2"></th>
+                        <th class="px-2"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach(Auth::user()->Complaint as $d)
+                    <tr class="border border-gray-200">
+                        <td class="text-center">{{$d->title}}</td>
+                        <td class="text-center">{{$d->comment}}</td>
+                        <td class="text-center">{{$d->latitude}}</td>
+                        <td class="text-center">{{$d->longitude}}</td>
+                        <td class="text-green-500"><a href="">Editar</a></td>
+                        <td class="text-red-500"><a href="">Excluir</a></td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            </div>
+       </div>
+    </div>
+    <!--div class="min-w-0 flex-auto px-4 sm:px-6 xl:px-8 pt-10 pb-24 lg:pb-16">
       <div class="rounded-t-xl overflow-hidden bg-gradient-to-r from-fuchsia-50 to-fuchsia-100 bg-gray-100 p-8">
           <div class="grid grid-cols-3 gap-4">      
            @foreach(Auth::user()->Complaint as $d)       
@@ -16,5 +47,5 @@
           <a href="/dashboard">Voltar</a>
           </div>
       </div>    
- </div>
+ </div-->
 </x-app-layout>
