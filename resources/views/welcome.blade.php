72809955a1f3507e5e76e4f4d95211aa981919a9
ThierryMatheus <thierrymatheusreal@gmail.com>
2021-11-25 13:04:47 -0300

    Bug da rota

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registrar-se</a>
                         @endif
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registrar-se como empresa</a>
+                        <a href="{{ route('registercompany') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registrar-se como empresa</a>
                     @endauth
                 </div>
             @endif
