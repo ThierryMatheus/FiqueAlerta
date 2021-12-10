301053fa2f3528be85e2a020c83f132368be06c0
ThierryMatheus <thierrymatheusreal@gmail.com>
2021-12-10 02:32:03 -0300

    GH-41 integracao do mapa na tela inical

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,27 @@
     <header>
         @include('layouts.header')
     </header>
+    <x-app-layout>
+            <div id="map" style="height: 400px;"></div>
+    </x-app-layout>
 
     @include('layouts.footer')
-
+    <script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDy_BoYMtuOPg56qqmooJ3fq1bX1TXKucw&callback=initMap&v=weekly"
+    async
+  ></script>
+  <script>
+    let map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+           center: {lat: -8.05428, lng: -34.8813},
+           zoom: 13,
+        });
+        const marker = new google.maps.Marker({
+          position: {lat: -8.05428, lng: -34.8813},
+          map: map,
+        });
+      };
+  </script>
 </body>
 </html>
