651227c04e39ef444df61e7e9219e16b50b9bb28
Esdras Pedro <esdraspedro@hotmail.com.br>
2021-12-18 20:14:34 -0300

    GH-38 - Alteração do visual da tela de escolha do tipo do usuário

--- resources/views/auth/user-type.blade.php
+++ resources/views/auth/user-type.blade.php
@@ -19,12 +19,13 @@
 </header>
 
 <main class="flex-grow">
-    <div class="flex justify-center mt-24 text-4xl font-bold">
-        <h1>Cadastre-se como:</h1>
+    <div class="flex justify-center mt-6 mb-7 text-3xl font-bold">
+        <h1>Escolha como quer se cadastrar</h1>
     </div>
     <div class="flex justify-center text-center">
-        <a href="/register" class="mt-64 ml-auto mr-96 text-2xl font-bold"><x-user-type-icon type="consumidor"/>Consumidor</a>
-            <a href="/register/company" class="mt-64 mr-auto ml-96 text-2xl font-bold"><x-user-type-icon type="empresa"/>Empresa</a>
+        <a href="/register" class="ml-auto text-2xl mr-32"><x-user-type-icon type="consumidor"/></a>
+        <div class="border-r-2 h-48 mt-28 border-gray-400"></div>
+        <a href="/register/company" class="mr-auto ml-24"><x-user-type-icon type="empresa"/></a>
     </div>
 </main>
 
