f453ebdd1c76a593ae9e9b38df664a063bd492e3
Ingrid-st <ingridfarias161@gmail.com>
2021-12-20 23:55:22 -0300

    GH-36 - Modificação visual da tela de cadastro do usuário

---
+++ resources/views/components/auth-card2.blade.php
@@ -0,0 +1,6 @@
+<div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+    <div class="w-full sm:max-w-xl mt-3 px-8 border overflow-hidden">
+        {{ $logo }}
+        {{ $slot }}
+    </div>
+</div>
