9372bd7d9ef6a7238842882a301d7d9bf63ce665
Huanderson Lima <mattewlima16@gmail.com>
2021-12-21 20:27:18 -0300

    GH-16 - Corrigindo estilo do componente input

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'appearance-none bg-transparent rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-0 px-2 leading-tight focus:outline-none']) !!}>
