a52a6017eb127146321e14ca4abcfe04d8addfd2
Esdras Pedro <esdraspedro@hotmail.com.br>
2021-12-23 10:06:21 -0300

    GH-47- Ajuste no código da barra de navegação
    Co-Authored-By: Ingrid Farias <80267843+Ingrid-st@users.noreply.github.com>

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,21 +1,21 @@
 <nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white sm:items-baseline w-full">
     <div class="mb-2 sm:mb-0">
-        <a href="/" class="text-2xl font-bold pl-16">
+        <a href="/" class="text-2xl font-bold ml-16">
             {{ __('Fique Alerta') }}
         </a>
     </div>
-    @if (Route::has('login'))
+    {{--@if (Route::has('login'))--}}
         <div class="pr-16 mr-2">
-            @auth
+            {{--@auth
                 <a href="{{ url('/dashboard') }}" class="text-base text-white bg-blue-750 px-10 p-2 rounded-md">Dashboard</a>
-            @else
+            @else--}}
                 <a href="{{ route('login') }}" class="text-sm font-bold text-blue-750 bg-white border border-blue-750 px-9 rounded-md">Entrar</a>
 
                 @if (Route::has('register'))
                     <a href="{{ route('type') }}" class="ml-4 text-sm font-bold text-white bg-blue-750 rounded-md py-1.5 px-11">Cadastrar</a>
                 @endif
-            @endauth
+            {{--@endauth--}}
         </div>
-    @endif
+    {{--@endif--}}
 </nav>
 
