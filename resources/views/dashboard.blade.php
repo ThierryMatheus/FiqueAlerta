4c46d8e8e6674d3cb4f55cb11eb260a96cd59c86
Esdras Pedro <esdraspedro@hotmail.com.br>
2021-12-28 10:42:37 -0300

    GH-41 - Modificação do visual da página do mapa
    Co-Authored-By: Ingrid Farias <80267843+Ingrid-st@users.noreply.github.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        @if (!Auth::user()->user_profile && !Auth::user()->company_profile)
-            <a href="/reclamante/create">Complete seu perfil</a>
-        @endif
-    </x-slot>
 
     {{--    <div class="min-w-0 flex-auto px-4 sm:px-6 xl:px-8 pt-10 pb-24 lg:pb-16">--}}
     {{--        <div class="rounded-t-xl overflow-hidden bg-gradient-to-r from-fuchsia-50 to-fuchsia-100 p-8">--}}
@@ -84,10 +79,6 @@
                                 </fieldset>
                                 <div class="categoriesSelected flex space-x-4 mt-5"></div>
                                 <div class="flex items-center justify-end mt-4">
-                                    <button
-                                        class="text-sm text-white bg-red-600 hover:bg-red-700 mr-1 rounded-md p-1 px-28 close-modal">
-                                        Cancel
-                                    </button>
                                     <x-button>Enviar</x-button>
                                 </div>
                             </form>
@@ -102,133 +93,185 @@
 
     <!-- preciso pensar num jeito de pegar o nome da categoria no javascript
         Vou criar um input hidden com o value que é um array de todas categorias selecionadas-->
+        <div class="flex sm:justify-between">
+            <div class="pr-6 pt-8 pl-7">
+                <h3 class="font-bold pb-10 text-xl">Filtros</h3>
 
-    <div class="p-6">
-        <legend class="text-center">Marque a Localização no mapa.</legend>
-        <div id="map" style="height: 650px;display: block;"></div>
-    </div>
+                <div>
+                    <p class="text-sm font-light pb-2">Localização</p>
+                    <div class="border-b border-b-4 border-black">
+                        <select name="type" id="type" class="block pt-0 border-none bg-transparent pr-56 pl-0 pb-0 font-light text-sm">
+                            <option value="1"></option>
+                            <option value="2"></option>
+                            <option value="3"></option>
+                        </select>
+                    </div>
+                </div>
 
-    <script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDy_BoYMtuOPg56qqmooJ3fq1bX1TXKucw&callback=initMap&v=weekly"
-        async
-    ></script>
-    <script>
-        let map;
-        let marker;
+                <div>
+                    <p class="text-sm font-light pb-3 pt-8">Distancia</p>
+                    <div>
+                        <input type="range" class="w-full">
+                    </div>
+                </div>
 
-        function initMap() {
-            map = new google.maps.Map(document.getElementById("map"), {
-                center: {lat: -8.05428, lng: -34.8813},
-                zoom: 13,
-            });
+                <div>
+                    <p class="text-sm font-light pb-3 pt-8">Categoria</p>
+                    <div class="border-b border-b-4 border-black">
+                        <select name="type" id="type" class="block pt-0 border-none bg-transparent pr-56 pl-0 pb-0 font-light text-sm">
+                            <option value="1"></option>
+                            <option value="2"></option>
+                            <option value="3"></option>
+                        </select>
+                    </div>
+                </div>
 
-            map.addListener('click', (data) => {
-                let lat = data.latLng.lat();
-                let lng = data.latLng.lng();
-                const modal = document.querySelector('.modal');
-                const closeModal = document.querySelectorAll('.close-modal');
+                <div>
+                    <p class="text-sm font-light pb-2 pt-8">Empresa</p>
+                    <div class="flex mt-3">
+                        <div class="flex w-full">
+                            <input type="checkbox" class="mr-1">
+                            <p class="text-xs">Públicas</p>
+                        </div>
+                        <div class="flex">
+                            <input type="checkbox" class="mr-1">
+                            <p class="text-xs mr-5">Privadas</p>
+                        </div> 
+                    </div>
+                </div>
+                <div class="mt-16 ml-12">
+                    <button class="text-sm text-white bg-blue-750 rounded-md p-1 px-10 text-sm font-bold">Aplicar</button>
+                </div>
 
-
-                if (marker && marker.setMap) {
-                    marker.setMap(null)
-                }
-
-                marker = new google.maps.Marker({
-                    position: {lat, lng},
-                    map: map,
-                    title: lat + " , " + lng,
-                });
-
-                /* logica do modal */
-
-                modal.classList.remove('hidden')
-
-                closeModal.forEach(close => {
-                    close.addEventListener('click', function () {
-                        modal.classList.add('hidden')
-                    });
-                });
-
-
-                var inputLat = document.getElementById("latitude");
-                var inputLng = document.getElementById("longitude");
-
-                //map.style.display = 'none';
-
-                inputLat.value = lat;
-                inputLng.value = lng;
-
-            });
-        };
-    </script>
-
-    <script>
-        let categoriesadded = []
-        let inp = document.getElementsByName("categorias")[0]
-
-        function addCategory() {
-
-            let value = document.getElementById("categorias").value;
-            let name = document.getElementById(value).text;
-
-            if (!categoriesadded.includes(value)) {
-                let categorySelected = document.createElement("p");
-                let title = document.createTextNode(name);
-                categorySelected.appendChild(title);
-                categoriesadded.push(value);
-                inp.value = categoriesadded;
-                categorySelected.setAttribute("id", "categoria" + value);
-                let cat = "categoria" + value
-                categorySelected.setAttribute("onclick", `removeCategory(${cat})`);
-
-                document.getElementsByClassName("categoriesSelected")[0].appendChild(categorySelected);
-
-            }
-
-
-        }
-
-        function removeCategory(category) {
-            let value = category.id.slice(-1)
-            let index = categoriesadded.indexOf(value);
-            if (index > -1) {
-                categoriesadded.splice(index, 1);
-                let categoryremoved = document.getElementById("categoria" + value);
-                document.getElementsByClassName("categoriesSelected")[0].removeChild(categoryremoved);
-                inp.value = categoriesadded;
-            }
-        }
-
-
-    </script>
-
-    <script>
-
-        $(function () {
-            $.ajax({
-                url: "{{ route('denuncia_all') }}",
-                type: "get",
-                data: $(this).serialize(),
-                dataType: 'json',
-                success: function (response) {
-                    var json = response;
-
-                    for (i = 0; i < json.length; i++) {
-                        var div = document.getElementById("complaint");
-                        var divDenuncia = document.createElement("div");
-                        var a = document.createElement("a");
-
-                        divDenuncia.className = 'h-12 flex items-center bg-gray-200 rounded-md text-center pl-2';
-                        divDenuncia.id = i;
-                        a.id = i + 'denuncia';
-                        a.innerHTML = json[i]["title"];
-
-
-                        div.appendChild(divDenuncia);
-                        divDenuncia.appendChild(a);
+            </div>
+            <div class="container relative">
+                <div>
+                    <div id="map" style="height: 595px; display: block;"></div>
+                </div>
+    
+                <script
+                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDy_BoYMtuOPg56qqmooJ3fq1bX1TXKucw&callback=initMap&v=weekly"
+                    async
+                ></script>
+                <script>
+                    let map;
+                    let marker;
+    
+                    function initMap() {
+                        map = new google.maps.Map(document.getElementById("map"), {
+                            center: {lat: -8.05428, lng: -34.8813},
+                            zoom: 13,
+                        });
+    
+                        map.addListener('click', (data) => {
+                            let lat = data.latLng.lat();
+                            let lng = data.latLng.lng();
+                            const modal = document.querySelector('.modal');
+                            const closeModal = document.querySelectorAll('.close-modal');
+    
+    
+                            if (marker && marker.setMap) {
+                                marker.setMap(null)
+                            }
+    
+                            marker = new google.maps.Marker({
+                                position: {lat, lng},
+                                map: map,
+                                title: lat + " , " + lng,
+                            });
+    
+                            /* logica do modal */
+    
+                            modal.classList.remove('hidden')
+    
+                            closeModal.forEach(close => {
+                                close.addEventListener('click', function () {
+                                    modal.classList.add('hidden')
+                                });
+                            });
+    
+    
+                            var inputLat = document.getElementById("latitude");
+                            var inputLng = document.getElementById("longitude");
+    
+                            //map.style.display = 'none';
+    
+                            inputLat.value = lat;
+                            inputLng.value = lng;
+    
+                        });
+                    };
+                </script>
+    
+                <script>
+                    let categoriesadded = []
+                    let inp = document.getElementsByName("categorias")[0]
+    
+                    function addCategory() {
+    
+                        let value = document.getElementById("categorias").value;
+                        let name = document.getElementById(value).text;
+    
+                        if (!categoriesadded.includes(value)) {
+                            let categorySelected = document.createElement("p");
+                            let title = document.createTextNode(name);
+                            categorySelected.appendChild(title);
+                            categoriesadded.push(value);
+                            inp.value = categoriesadded;
+                            categorySelected.setAttribute("id", "categoria" + value);
+                            let cat = "categoria" + value
+                            categorySelected.setAttribute("onclick", `removeCategory(${cat})`);
+    
+                            document.getElementsByClassName("categoriesSelected")[0].appendChild(categorySelected);
+    
+                        }
+    
+    
                     }
-                }
-            });
-        });
-    </script>
+    
+                    function removeCategory(category) {
+                        let value = category.id.slice(-1)
+                        let index = categoriesadded.indexOf(value);
+                        if (index > -1) {
+                            categoriesadded.splice(index, 1);
+                            let categoryremoved = document.getElementById("categoria" + value);
+                            document.getElementsByClassName("categoriesSelected")[0].removeChild(categoryremoved);
+                            inp.value = categoriesadded;
+                        }
+                    }
+    
+    
+                </script>
+    
+                <script>
+    
+                    $(function () {
+                        $.ajax({
+                            url: "{{ route('denuncia_all') }}",
+                            type: "get",
+                            data: $(this).serialize(),
+                            dataType: 'json',
+                            success: function (response) {
+                                var json = response;
+    
+                                for (i = 0; i < json.length; i++) {
+                                    var div = document.getElementById("complaint");
+                                    var divDenuncia = document.createElement("div");
+                                    var a = document.createElement("a");
+    
+                                    divDenuncia.className = 'h-12 flex items-center bg-gray-200 rounded-md text-center pl-2';
+                                    divDenuncia.id = i;
+                                    a.id = i + 'denuncia';
+                                    a.innerHTML = json[i]["title"];
+    
+    
+                                    div.appendChild(divDenuncia);
+                                    divDenuncia.appendChild(a);
+                                }
+                            }
+                        });
+                    });
+                </script>
+            </div>
+        </div>
 </x-app-layout>
