2f84be85f02c9cad7ab2548f7b85fbe87487e895
Ingrid-st <ingridfarias161@gmail.com>
2021-12-30 10:52:25 -0300

    GH-40 - Alteração no código de listar denúncia

--- resources/views/denuncia/list.blade.php
+++ resources/views/denuncia/list.blade.php
@@ -28,43 +28,56 @@
                     </th>
                 </tr>
                 <tr class="grid gap-32 grid-cols-6 border-b border-b-4 border-gray-200">
-                    <th class="font-medium text-sm text-left text-gray-500 py-4">Título</th>
-                    <th class="font-medium text-sm text-left text-gray-500 py-4">Descrição</th>
-                    <th class="font-medium text-sm text-left text-gray-500 py-4">Categoria</th>
-                    <th class="font-medium text-sm text-left text-gray-500 py-4">Data</th>
-                    <th class="font-medium text-sm text-left text-gray-500 py-4">Hora</th>
-                    <th class="py-4"></th>
+                    <th class="font-medium mt-1 text-sm text-left text-gray-500 py-3">Título</th>
+                    <th class="font-medium mt-1 text-sm text-left text-gray-500 py-3">Descrição</th>
+                    <th class="font-medium mt-1 text-sm text-left text-gray-500 py-3">Categoria</th>
+                    <th class="font-medium mt-1 text-sm text-left text-gray-500 py-3">Data</th>
+                    <th class="font-medium mt-1 text-sm text-left text-gray-500 py-3">Hora</th>
+                    <th class="py-3"></th>
                 </tr>
                 </thead>
 
                 <tbody class="pt-8">
                 @foreach(Auth::user()->Complaint as $denuncia)
 
-                    <tr class="border-b border-b-4 border-gray-200 py-4">
+                    <tr class="border-b border-b-4 border-gray-200 py-3">
 
-                        <td class="text-center py-4">{{$denuncia->title}}</td>
-                        <td class="text-center py-4">{{$denuncia->comment}}</td>
+                        <td class="text-center mt-1 py-3">{{$denuncia->title}}</td>
+                        <td class="text-center mt-1 py-3">{{$denuncia->comment}}</td>
 
                         @php 
                         $categorias = $denuncia::find($denuncia->id)->categories;
                         @endphp
 
                         @foreach($categorias as $categoria)
-                            <td class="text-center py-4">{{$categoria->name}}</td>
+                            <td class="text-center mt-1 py-3">{{$categoria->name}}</td>
                         @endforeach
 
-                        <td class="text-center py-4">{{$denuncia->claim_date}}</td>
-                        {{--<td class="text-center py-4">{{$denuncia->}}</td>--}}
+                        <td class="text-center mt-1 py-3">{{$denuncia->claim_date}}</td>
+                        {{--<td class="text-center mt-1 py-3">{{$denuncia->}}</td>--}}
 
-                        <td class="text-green-500"><a href="/denuncia/{{$denuncia->id}}/edit">Editar</a></td>
-                        <td>
-                            <form action="/denuncia/{{ $denuncia->id }}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="text-red-500"
-                                        onclick="return confirm('Tem certeza que deseja excluir?')">Excluir
-                                </button>
-                            </form>
+                        <td class="py-3 px-7">
+                            
+                            <x-dropdown>
+                                <x-slot name="trigger" class="">
+                                    <button>
+                                        <svg width="4" height="15" viewBox="0 0 4 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M3.119 12.9532C3.119 13.3386 2.98867 13.6672 2.728 13.9392C2.456 14.2112 2.116 14.3472 1.708 14.3472C1.28867 14.3472 0.948667 14.2169 0.688 13.9562C0.427333 13.6842 0.297 13.3499 0.297 12.9532C0.297 12.5566 0.427333 12.2279 0.688 11.9672C0.948667 11.6952 1.28867 11.5592 1.708 11.5592C2.116 11.5592 2.456 11.6952 2.728 11.9672C2.98867 12.2392 3.119 12.5679 3.119 12.9532ZM3.119 7.50461C3.119 7.88994 2.98867 8.21861 2.728 8.49061C2.456 8.76261 2.116 8.89861 1.708 8.89861C1.28867 8.89861 0.948667 8.76827 0.688 8.50761C0.427333 8.23561 0.297 7.90127 0.297 7.50461C0.297 7.10794 0.427333 6.77927 0.688 6.51861C0.948667 6.24661 1.28867 6.11061 1.708 6.11061C2.116 6.11061 2.456 6.24661 2.728 6.51861C2.98867 6.79061 3.119 7.11927 3.119 7.50461ZM3.119 2.05597C3.119 2.44131 2.98867 2.76997 2.728 3.04197C2.456 3.31397 2.116 3.44997 1.708 3.44997C1.28867 3.44997 0.948667 3.31964 0.688 3.05897C0.427333 2.78697 0.297 2.45264 0.297 2.05597C0.297 1.65931 0.427333 1.33064 0.688 1.06997C0.948667 0.797974 1.28867 0.661974 1.708 0.661974C2.116 0.661974 2.456 0.797974 2.728 1.06997C2.98867 1.34197 3.119 1.67064 3.119 2.05597Z" fill="black"/>
+                                        </svg>
+                                    </button>
+                                </x-slot>
+            
+                                <x-slot name="content" class="w-0">
+                                            <button class="w-full text-left font-medium text-sm block px-3 py-2 text-sm leading-5 text-gray-600 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"><a href="/denuncia/{{$denuncia->id}}/edit">Editar</a></button>
+                                            
+                                            <form action="/denuncia/{{ $denuncia->id }}" method="post" class="text-left block px-3 py-2 text-sm leading-5 text-gray-600 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" onclick="return confirm('Tem certeza que deseja excluir?')">Excluir</button>
+                                            </form>
+                                </x-slot>
+                            </x-dropdown>
+
                         </td>
                     </tr>
                 @endforeach
